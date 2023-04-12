7b1c750846ba400feff69ce2f6ed7552718b5dea
altarev_da <altarev_da@nlmk.com>
2023-04-12 05:18:47 +0300

    apdat h_w_1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import *
+
+def tests_calculate_total_price():
+    price = 80
+    quentity = 1000
+    test_1 = Item("cola", price, quentity)
+    assert test_1.calculate_total_price() == price * quentity
+
+def tests_apply_discount():
+    price = 100
+    quentity = 10
+    test_2 = Item("frutela", price, quentity)
+    assert test_2.apply_discount() == price * 0.85
