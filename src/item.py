d27568ea682d66241fdaf2f8f9c717dd9825f086
altarev_da <altarev_da@nlmk.com>
2023-04-20 21:40:32 +0300

    final_h_w_2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,12 +15,38 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
         
+    @property
+    def name(self):
+        return self.__name
 
+    @name.setter
+    def name(self, par_name):
+        if len(par_name) <= 10:
+            self.__name = par_name
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        cls.all = []
+        with open("src/items.csv") as file:
+            reader = csv.DictReader(file)
+            for line in reader:
+                Item(line["name"], line["price"], line["quantity"])
+
+    @staticmethod
+    def string_to_number(string):
+        if "." in string:
+            item = string.split(".")
+            result = item[0]
+            result = int(result)
+        else:
+            result = int(string)
+        return result
+        
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
@@ -33,5 +61,4 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.pay_rate * self.price
-        
-        
+        return self.price
--- tests/test_item.py
+++ tests/test_item.py
@@ -12,3 +12,24 @@ def tests_apply_discount():
     quentity = 10
     test_2 = Item("frutela", price, quentity)
     assert test_2.apply_discount() == price * 0.85
+
+def tests_name():
+    price = 100
+    quentity = 10
+    test_3 = Item("barny", price, quentity)
+    assert test_3.name == "barny"
+    test_3.name = "jack"
+    assert test_3.name == "jack"
+
+def tests_instantiate_from_csv():
+    test_4 = Item("barny", 100, 10)
+    test_4.instantiate_from_csv()
+    assert len(test_4.all) == 5
+
+def tests_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+
+
